f981ca1fd6160a05eadb5b3e0007548810dced12
panstromek <panstromek@seznam.cz>
2020-07-22 19:36:50 +0200

    move add_hash to engine

--- engine/src/hash.rs
+++ engine/src/hash.rs
@@ -510,3 +510,84 @@ pub unsafe fn setup_hash(mut clear: i32) {
         j += 1
     };
 }
+
+
+/*
+   ADD_HASH
+   Add information to the hash table. Two adjacent positions are tried
+   and the most shallow search is replaced.
+*/
+
+pub unsafe fn add_hash(mut reverse_mode: i32,
+                       mut score: i32,
+                       mut best: i32,
+                       mut flags: i32,
+                       mut draft: i32,
+                       mut selectivity: i32) {
+    let mut old_draft: i32 = 0;
+    let mut change_encouragment: i32 = 0;
+    let mut index: u32 = 0;
+    let mut index1: u32 = 0;
+    let mut index2: u32 = 0;
+    let mut code1: u32 = 0;
+    let mut code2: u32 = 0;
+    let mut entry =
+        HashEntry{key1: 0,
+            key2: 0,
+            eval: 0,
+            move_0: [0; 4],
+            draft: 0,
+            selectivity: 0,
+            flags: 0,};
+    // TODO
+    //  note for investigation. There was this assert in the original source
+    //      assert( abs( score ) != SEARCH_ABORT );
+    //  but SEARCH_ABORT is defined as
+    //      #define SEARCH_ABORT                -27000
+    //  so it cannot ever fail...
+    //  And there is not comment or anything in there about it.. weird
+    //
+
+    if reverse_mode != 0 {
+        code1 = hash2 ^ hash_trans2;
+        code2 = hash1 ^ hash_trans1
+    } else { code1 = hash1 ^ hash_trans1; code2 = hash2 ^ hash_trans2 }
+    index1 = code1 & hash_mask as u32;
+    index2 = index1 ^ 1 as i32 as u32;
+    if (*hash_table.offset(index1 as isize)).key2 == code2 {
+        index = index1
+    } else if (*hash_table.offset(index2 as isize)).key2 == code2 {
+        index = index2
+    } else if (*hash_table.offset(index1 as
+        isize)).key1_selectivity_flags_draft &
+        255 as i32 as u32 <=
+        (*hash_table.offset(index2 as
+            isize)).key1_selectivity_flags_draft
+            & 255 as i32 as u32 {
+        index = index1
+    } else { index = index2 }
+    old_draft =
+        ((*hash_table.offset(index as isize)).key1_selectivity_flags_draft &
+            255 as i32 as u32) as i32;
+    if flags & 4 as i32 != 0 {
+        /* Exact scores are potentially more useful */
+        change_encouragment = 2 as i32
+    } else { change_encouragment = 0 as i32 }
+    if (*hash_table.offset(index as isize)).key2 == code2 {
+        if old_draft > draft + change_encouragment + 2 as i32 {
+            return
+        }
+    } else if old_draft > draft + change_encouragment + 4 as i32 {
+        return
+    }
+    entry.key1 = code1;
+    entry.key2 = code2;entry.eval = score;
+    entry.move_0[0 as i32 as usize] = best;
+    entry.move_0[1 as i32 as usize] = 0 as i32;
+    entry.move_0[2 as i32 as usize] = 0 as i32;
+    entry.move_0[3 as i32 as usize] = 0 as i32;
+    entry.flags = flags as i16;
+    entry.draft = draft as i16;
+    entry.selectivity = selectivity as i16;
+    wide_to_compact(&mut entry, &mut *hash_table.offset(index as isize));
+}
