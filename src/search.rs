75b3e3f832572001d1740a038fff27339810a151
panstromek <panstromek@seznam.cz>
2020-07-25 17:45:11 +0200

    move rest of the search

--- src/search.rs
+++ src/search.rs
@@ -15,64 +15,24 @@ pub use ::engine::src::search::*;
 
    Contents:      Common search routines and variables.
 */
-/* Global variables */
-/*
-  COMPLETE_PV
-  Complete the principal variation with passes (if any there are any).
-*/
-
-pub unsafe fn complete_pv(mut side_to_move: i32) {
-    let mut i: i32 = 0;
-    let mut actual_side_to_move: [i32; 60] = [0; 60];
-    full_pv_depth = 0 as i32;
-    i = 0 as i32;
-    while i < pv_depth[0 as i32 as usize] {
-        if make_move(side_to_move, pv[0 as i32 as usize][i as usize],
-                     1 as i32) != 0 {
-            actual_side_to_move[i as usize] = side_to_move;
-            full_pv[full_pv_depth as usize] =
-                pv[0 as i32 as usize][i as usize];
-            full_pv_depth += 1
-        } else {
-            full_pv[full_pv_depth as usize] = -(1 as i32);
-            full_pv_depth += 1;
-            side_to_move = 0 as i32 + 2 as i32 - side_to_move;
-            if make_move(side_to_move,
-                         pv[0 as i32 as usize][i as usize],
-                         1 as i32) != 0 {
-                actual_side_to_move[i as usize] = side_to_move;
-                full_pv[full_pv_depth as usize] =
-                    pv[0 as i32 as usize][i as usize];
-                full_pv_depth += 1
-            } else {
-                let mut j: i32 = 0;
-                printf(b"pv_depth[0] = %d\n\x00" as *const u8 as
-                           *const i8,
-                       pv_depth[0 as i32 as usize]);
-                j = 0 as i32;
-                while j < pv_depth[0 as i32 as usize] {
-                    printf(b"%c%c \x00" as *const u8 as *const i8,
-                           'a' as i32 +
-                               pv[0 as i32 as usize][j as usize] %
-                                   10 as i32 - 1 as i32,
-                           '0' as i32 +
-                               pv[0 as i32 as usize][j as usize] /
-                                   10 as i32);
-                    j += 1
-                }
-                puts(b"\x00" as *const u8 as *const i8);
-                printf(b"i=%d\n\x00" as *const u8 as *const i8, i);
-                fatal_error(b"Error in PV completion\x00" as *const u8 as
-                                *const i8);
-            }
-        }
-        side_to_move = 0 as i32 + 2 as i32 - side_to_move;
-        i += 1
+#[no_mangle]
+pub unsafe extern "C" fn handle_fatal_pv_error(i: i32) {
+    printf(b"pv_depth[0] = %d\n\x00" as *const u8 as
+               *const i8,
+           pv_depth[0 as i32 as usize]);
+    let mut j = 0 as i32;
+    while j < pv_depth[0 as i32 as usize] {
+        printf(b"%c%c \x00" as *const u8 as *const i8,
+               'a' as i32 +
+                   pv[0 as i32 as usize][j as usize] %
+                       10 as i32 - 1 as i32,
+               '0' as i32 +
+                   pv[0 as i32 as usize][j as usize] /
+                       10 as i32);
+        j += 1
     }
-    i = pv_depth[0 as i32 as usize] - 1 as i32;
-    while i >= 0 as i32 {
-        unmake_move(actual_side_to_move[i as usize],
-                    pv[0 as i32 as usize][i as usize]);
-        i -= 1
-    };
+    puts(b"\x00" as *const u8 as *const i8);
+    printf(b"i=%d\n\x00" as *const u8 as *const i8, i);
+    fatal_error(b"Error in PV completion\x00" as *const u8 as
+        *const i8);
 }
