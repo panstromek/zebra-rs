bfe2e97fa46fddeb2d8662838b2e21888a3ae08c
panstromek <panstromek@seznam.cz>
2020-08-06 20:46:52 +0200

    initial generic plumbing for play_game

--- src/zebra.rs
+++ src/zebra.rs
@@ -889,10 +889,16 @@ unsafe fn play_game(mut file_name: *const i8,
                     mut repeat: i32, log_file_name_: *mut i8) {
     let mut move_file = LibcFileMoveSource::open(move_file_name);
 
-    engine_play_game(file_name, move_string, repeat, log_file_name_, move_file)
+    engine_play_game::<LibcFrontend, _>(file_name, move_string, repeat, log_file_name_, move_file)
 }
+struct LibcFrontend {}
+trait ZebraFrontend {}
+impl ZebraFrontend for LibcFrontend {}
 
-unsafe fn engine_play_game(mut file_name: *const i8, mut move_string: *const i8, mut repeat: i32, log_file_name_: *mut i8, mut move_file: Option<LibcFileMoveSource>) {
+unsafe fn engine_play_game<ZF: ZebraFrontend, Source: InitialMoveSource>(
+    mut file_name: *const i8, mut move_string: *const i8,
+    mut repeat: i32, log_file_name_: *mut i8,
+    mut move_file: Option<Source>) {
     let mut eval_info =
         EvaluationType{type_0: MIDGAME_EVAL,
                        res: WON_POSITION,
