16fcca6e9dd7e59fbb0096e193aaf50094253e6b
panstromek <panstromek@seznam.cz>
2020-08-13 10:26:00 +0200

    add wasm-simple crate for testing + few little changes

---
+++ wasm-simple/src/lib.rs
@@ -0,0 +1,24 @@
+use wasm_bindgen::prelude::*;
+use engine::src::game::{engine_game_init, engine_global_setup};
+use engine::src::getcoeff::CoeffSource;
+extern crate engine;
+extern crate libc;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+struct CoeffSourceWasm;
+impl CoeffSource for CoeffSourceWasm {
+    fn next_word(&mut self) -> i16 {
+        // unimplemented!()
+        0
+    }
+}
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    let mut coeffs = CoeffSourceWasm {};
+    unsafe { engine_global_setup(0, 7, None, coeffs); }
+
+    alert(&format!("Hello, {}!", name));
+}
