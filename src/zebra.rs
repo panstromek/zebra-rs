6cac926a479caf5f8440fbc1e9d5f9a584b44c70
panstromek <panstromek@seznam.cz>
2020-08-13 12:24:15 +0200

    don't allow mutable transmutes

--- src/zebra.rs
+++ src/zebra.rs
@@ -1,4 +1,4 @@
-#![allow(dead_code, mutable_transmutes, non_camel_case_types, non_snake_case,
+#![allow(dead_code, non_camel_case_types, non_snake_case,
          non_upper_case_globals, unused_assignments, unused_mut)]
 
 use std::process::exit;
