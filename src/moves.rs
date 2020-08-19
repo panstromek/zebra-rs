f7945ce7b0c1159234415bbf131d925cb85ba73e
panstromek <panstromek@seznam.cz>
2020-08-19 20:00:41 +0200

    extract prompt_get_move

--- src/moves.rs
+++ src/moves.rs
@@ -17,22 +17,12 @@ pub use engine::src::moves::*;
    GET_MOVE
    Prompts the user to enter a move and checks if the move is legal.
 */
-pub unsafe fn get_move(side_to_move: i32)
- -> i32 {
+pub unsafe fn get_move(side_to_move: i32) -> i32 {
     let mut buffer: [i8; 255] = [0; 255];
     let mut ready = 0 as i32;
     let mut curr_move: i32 = 0;
     while ready == 0 {
-        if side_to_move == 0 as i32 {
-            printf(b"%s: \x00" as *const u8 as *const i8,
-                   b"Black move\x00" as *const u8 as *const i8);
-        } else {
-            printf(b"%s: \x00" as *const u8 as *const i8,
-                   b"White move\x00" as *const u8 as *const i8);
-        }
-        scanf(b"%s\x00" as *const u8 as *const i8,
-              buffer.as_mut_ptr());
-        curr_move = atoi(buffer.as_mut_ptr());
+        prompt_get_move(side_to_move, &mut buffer);
         ready = valid_move(curr_move, side_to_move);
         if ready == 0 {
             curr_move =
@@ -45,4 +35,18 @@ pub unsafe fn get_move(side_to_move: i32)
         }
     }
     curr_move
+}
+
+fn prompt_get_move(side_to_move: i32, buffer: &mut [i8; 255]) -> i32 {
+    unsafe {
+        if side_to_move == 0 as i32 {
+            printf(b"%s: \x00" as *const u8 as *const i8,
+                   b"Black move\x00" as *const u8 as *const i8);
+        } else {
+            printf(b"%s: \x00" as *const u8 as *const i8,
+                   b"White move\x00" as *const u8 as *const i8);
+        }
+        scanf(b"%s\x00" as *const u8 as *const i8, buffer.as_mut_ptr());
+        atoi(buffer.as_mut_ptr())
+    }
 }
