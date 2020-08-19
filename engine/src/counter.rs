87388decb0769093fdb0b947a454af02e1b400dd
panstromek <panstromek@seznam.cz>
2020-08-19 19:39:52 +0200

    remove unsafety from counter.rs (assuming no one uses it for something dirty for now)

--- engine/src/counter.rs
+++ engine/src/counter.rs
@@ -8,7 +8,7 @@ pub struct CounterType {
   RESET_COUNTER
 */
 
-pub unsafe fn reset_counter(mut counter: *mut CounterType) {
+pub fn reset_counter(counter: &mut CounterType) {
     (*counter).lo = 0 as i32 as u32;
     (*counter).hi = 0 as i32 as u32;
 }
@@ -17,7 +17,7 @@ pub unsafe fn reset_counter(mut counter: *mut CounterType) {
   Makes sure that the LO part of the counter only contains 8 decimal digits.
 */
 
-pub unsafe fn adjust_counter(mut counter: *mut CounterType) {
+pub fn adjust_counter(counter: &mut CounterType) {
     while (*counter).lo >= 100000000 as i32 as u32 {
         (*counter).lo =
             (*counter).lo.wrapping_sub(100000000 as i32 as
@@ -30,18 +30,16 @@ pub unsafe fn adjust_counter(mut counter: *mut CounterType) {
   Converts a counter to a single floating-point value.
 */
 
-pub unsafe fn counter_value(counter: *mut CounterType)
+pub fn counter_value(counter: &mut CounterType)
  -> f64 {
     adjust_counter(counter);
-    return 100000000 as i32 as f64 *
-               (*counter).hi as f64 +
-               (*counter).lo as f64;
+    100000000f64 * (*counter).hi as f64 + (*counter).lo as f64
 }
 /*
   ADD_COUNTER
   Adds the value of the counter TERM to the counter SUM.
 */
-pub unsafe fn add_counter(sum: *mut CounterType, term: *mut CounterType) {
+pub fn add_counter(sum: &mut CounterType, term: &mut CounterType) {
     (*sum).lo = (*sum).lo.wrapping_add((*term).lo);
     (*sum).hi = (*sum).hi.wrapping_add((*term).hi);
     adjust_counter(sum);
