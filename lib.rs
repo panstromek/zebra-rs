951dcefc1704f71fb38f9d56046d62d9f381079d
panstromek <panstromek@seznam.cz>
2020-08-25 13:33:00 +0200

    remove unused modules

--- lib.rs
+++ lib.rs
@@ -11,17 +11,12 @@ extern crate libc;
 
 pub mod src {
     pub mod display;
-    pub mod end;
     pub mod error;
     pub mod game;
     pub mod getcoeff;
     pub mod learn;
-    pub mod midgame;
-    pub mod moves;
     pub mod osfbook;
-    pub mod search;
     pub mod thordb;
-    pub mod timer;
     pub mod stubs;
     pub mod zebra;
 }
