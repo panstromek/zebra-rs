9d38e478065ce1db2da12bbda3409f12009546a4
panstromek <panstromek@seznam.cz>
2020-08-29 20:30:08 +0200

    move top level crate into a new "legacy-zebra" crate

---
+++ legacy-zebra/src/main.rs
@@ -0,0 +1,5 @@
+use legacy_zebra::src::zebra::main as main_lib;
+
+fn main () {
+    main_lib();
+}
--- src/main.rs
+++
@@ -1,5 +0,0 @@
-use c2rust_out::src::zebra::main as main_lib;
-
-fn main () {
-    main_lib();
-}
