56d254f29e02de785898b0a300905b7aea318f99
panstromek <panstromek@seznam.cz>
2020-10-17 17:33:26 +0200

    replacer pointers with slices in flip_direction globals

--- engine/src/cntflip.rs
+++ engine/src/cntflip.rs
@@ -6,7 +6,7 @@
    Last modified:   December 25, 1999
 */
 pub unsafe fn AnyFlips_compact(board: &mut [i32; 128],
-                               first_flip_direction: &mut [*mut i32; 100],
+                               first_flip_direction: &[&[i32]; 100],
                                sqnum: i32, color: i32, oppcol: i32) -> i32 {
 
     let sq = &board[sqnum as usize];
@@ -14,7 +14,7 @@ pub unsafe fn AnyFlips_compact(board: &mut [i32; 128],
     loop  {
         let any_drctnl_flips = {
             let sq: &i32 = sq;
-            let inc: i32 = *inc;
+            let inc: i32 = inc[0];
             let sq = sq as *const i32;
             let mut pt = sq.offset(inc as isize);
             if *pt == oppcol {
@@ -42,8 +42,8 @@ pub unsafe fn AnyFlips_compact(board: &mut [i32; 128],
         if any_drctnl_flips != 0 {
             return 1 as i32
         }
-        inc = inc.offset(1);
-        if !(*inc != 0) { break ; }
+        inc = &inc[1..];// .offset(1);
+        if !(inc[0] != 0) { break ; }
     }
     0
 }
