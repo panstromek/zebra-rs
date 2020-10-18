6972a82fed82db56474a9f63c7f9953578f9c34e
panstromek <panstromek@seznam.cz>
2020-10-18 23:10:57 +0200

    mvoe ponder move to engine

--- engine/src/game.rs
+++ engine/src/game.rs
@@ -1,14 +1,14 @@
 use crate::src::zebra::EvaluationType;
 use crate::src::counter::{adjust_counter, counter_value, reset_counter, add_counter};
-use crate::src::search::{nodes, total_time, total_evaluations, total_nodes, setup_search, disc_count, complete_pv, get_ponder_move, evaluations, set_current_eval, create_eval_info, root_eval, force_return, clear_pv, evals};
+use crate::src::search::{nodes, total_time, total_evaluations, total_nodes, setup_search, disc_count, complete_pv, get_ponder_move, evaluations, set_current_eval, create_eval_info, root_eval, force_return, clear_pv, evals, clear_ponder_move, set_ponder_move, float_move, sort_moves};
 use crate::src::globals::{pv_depth, pv, board, score_sheet_row, black_moves, piece_count};
 use crate::src::osfbook::{clear_osf, get_book_move, fill_move_alternatives, check_forced_opening};
 use crate::src::getcoeff::{clear_coeffs, post_init_coeffs, eval_adjustment, init_coeffs_calculate_patterns, process_coeffs_from_fn_source, init_memory_handler, CoeffAdjustments, remove_coeffs};
-use crate::src::hash::{free_hash, determine_hash_values, init_hash};
-use crate::src::timer::{clear_ponder_times, init_timer, time_t, clear_panic_abort, get_elapsed_time, is_panic_abort, determine_move_time};
+use crate::src::hash::{free_hash, determine_hash_values, init_hash, find_hash, HashEntry};
+use crate::src::timer::{clear_ponder_times, init_timer, time_t, clear_panic_abort, get_elapsed_time, is_panic_abort, determine_move_time, toggle_abort_check, ponder_depth, add_ponder_time, get_real_timer, start_move};
 use crate::src::end::{setup_end, end_game};
 use crate::src::midgame::{setup_midgame, is_midgame_abort, middle_game, toggle_midgame_hash_usage, toggle_midgame_abort_check, clear_midgame_abort, calculate_perturbation};
-use crate::src::moves::{disks_played, valid_move, move_list, move_count, generate_all};
+use crate::src::moves::{disks_played, valid_move, move_list, move_count, generate_all, unmake_move, make_move};
 use crate::src::stable::init_stable;
 use crate::src::probcut::init_probcut;
 use crate::src::myrandom::{my_srandom, my_random};
@@ -756,6 +756,177 @@ pub unsafe fn generic_compute_move<L: ComputeMoveLogger, Out: ComputeMoveOutput,
     }
     return curr_move;
 }
+
+/*
+   COMPUTE_MOVE
+   Returns the best move in a position given search parameters.
+*/
+pub unsafe fn compute_move<L: ComputeMoveLogger, Out: ComputeMoveOutput, FE: FrontEnd, Thor: ThorDatabase>(
+    side_to_move: i32,
+    update_all: i32,
+    my_time: i32,
+    my_incr: i32,
+    timed_depth: i32,
+    book: i32,
+    mid: i32,
+    exact: i32,
+    wld: i32,
+    search_forced: i32,
+    eval_info: &mut EvaluationType)
+    -> i32 {
+    return generic_compute_move::<L, Out, FE, Thor>(
+        side_to_move, update_all, my_time,
+        my_incr, timed_depth,
+        book, mid,
+        exact, wld,
+        search_forced, eval_info, &mut L::create_log_file_if_needed());
+}
+
+pub unsafe fn ponder_move<
+    L: ComputeMoveLogger,
+    Out: ComputeMoveOutput,
+    FE: FrontEnd,
+    Thor: ThorDatabase,
+    Rep: PonderMoveReport>(side_to_move: i32,
+                           _book: i32,
+                           mid: i32,
+                           exact: i32,
+                           wld: i32) {
+    let mut eval_info =
+        EvaluationType{type_0: MIDGAME_EVAL,
+            res: WON_POSITION,
+            score: 0,
+            confidence: 0.,
+            search_depth: 0,
+            is_book: 0,};
+    let mut entry =
+        HashEntry{key1: 0,
+            key2: 0,
+            eval: 0,
+            move_0: [0; 4],
+            draft: 0,
+            selectivity: 0,
+            flags: 0,};
+    let mut move_start_time: f64 = 0.;
+    let mut move_stop_time: f64 = 0.;
+    let mut i: i32 = 0;
+    let mut j: i32 = 0;
+    let mut this_move: i32 = 0;
+    let mut expect_count: i32 = 0;
+    let mut expect_list: [i32; 64] = [0; 64];
+    let mut best_pv: [i32; 61] = [0; 61];
+    /* Disable all time control mechanisms as it's the opponent's
+       time we're using */
+    toggle_abort_check(0 as i32);
+    toggle_midgame_abort_check(0 as i32);
+    start_move::<FE>(0 as i32 as f64,
+                     0 as i32 as f64,
+                     disc_count(0 as i32, &board) + disc_count(2 as i32, &board));
+    clear_ponder_times();
+    determine_hash_values(side_to_move, &board);
+    reset_counter(&mut nodes);
+    /* Find the scores for the moves available to the opponent. */
+    let mut hash_move = 0;
+    find_hash(&mut entry, 1 as i32);
+    if entry.draft as i32 != 0 as i32 {
+        hash_move = entry.move_0[0]
+    } else {
+        find_hash(&mut entry, 0 as i32);
+        if entry.draft as i32 != 0 as i32 {
+            hash_move = entry.move_0[0]
+        }
+    }
+    let stored_echo = echo;
+    echo = 0;
+    compute_move::<L, Out, FE, Thor>(side_to_move, 0 as i32, 0 as i32,
+                 0 as i32, 0 as i32, 0 as i32,
+                 if (8 as i32) < mid {
+                     8 as i32
+                 } else { mid }, 0 as i32, 0 as i32,
+                 0 as i32, &mut eval_info);
+    echo = stored_echo;
+    /* Sort the opponents on the score and push the table move (if any)
+       to the front of the list */
+    if force_return != 0 {
+        expect_count = 0 as i32
+    } else {
+        sort_moves(move_count[disks_played as usize]);
+        float_move(hash_move, move_count[disks_played as usize]);
+        expect_count = move_count[disks_played as usize];
+        i = 0;
+        while i < expect_count {
+            expect_list[i as usize] =
+                move_list[disks_played as usize][i as usize];
+            i += 1
+        }
+        Rep::report_hash_move(hash_move);
+        let move_list_item = &move_list[disks_played as usize];
+        let evals_item = &evals[disks_played as usize];
+        Rep::report_move_evals(expect_count, move_list_item, evals_item);
+    }
+    /* Go through the expected moves in order and prepare responses. */
+    let mut best_pv_depth = 0;
+    let mut i = 0;
+    while force_return == 0 && i < expect_count {
+        move_start_time = get_real_timer::<FE>();
+        set_ponder_move(expect_list[i as usize]);
+        this_move = expect_list[i as usize];
+        prefix_move = this_move;
+        make_move(side_to_move, this_move, 1 as i32);
+        compute_move::<L, Out, FE, Thor>(0 as i32 + 2 as i32 - side_to_move,
+                                         0 as i32, 0 as i32, 0 as i32,
+                     1 as i32, 0 as i32, mid, exact, wld,
+                     0 as i32, &mut eval_info);
+        unmake_move(side_to_move, this_move);
+        clear_ponder_move();
+        move_stop_time = get_real_timer::<FE>();
+        add_ponder_time(expect_list[i as usize],
+                        move_stop_time - move_start_time);
+        ponder_depth[expect_list[i as usize] as usize] =
+            if ponder_depth[expect_list[i as usize] as usize] >
+                max_depth_reached - 1 as i32 {
+                ponder_depth[expect_list[i as usize] as usize]
+            } else { (max_depth_reached) - 1 as i32 };
+        if i == 0 as i32 && force_return == 0 {
+            /* Store the PV for the first move */
+            best_pv_depth = pv_depth[0];
+            j = 0;
+            while j < pv_depth[0] {
+                best_pv[j as usize] =
+                    pv[0][j as usize];
+                j += 1
+            }
+        }
+        i += 1
+    }
+    /* Make sure the PV looks reasonable when leaving - either by
+       clearing it altogether or, preferrably, using the stored PV for
+       the first move if it is available. */
+    max_depth_reached += 1;
+    prefix_move = 0;
+    if best_pv_depth == 0 as i32 {
+        pv_depth[0] = 0 as i32
+    } else {
+        pv_depth[0] =
+            best_pv_depth + 1 as i32;
+        pv[0][0] =
+            expect_list[0];
+        i = 0;
+        while i < best_pv_depth {
+            pv[0][(i + 1 as i32) as usize] =
+                best_pv[i as usize];
+            i += 1
+        }
+    }
+    /* Don't forget to enable the time control mechanisms when leaving */
+    toggle_abort_check(1 as i32);
+    toggle_midgame_abort_check(1 as i32);
+}
+pub trait PonderMoveReport {
+    fn report_move_evals(expect_count: i32, move_list_item: &[i32; 64], evals_item: &[i32; 128]);
+    fn report_hash_move(hash_move: i32);
+}
+
 pub trait ComputeMoveOutput {
     fn display_out_optimal_line();
     fn send_move_type_0_status(interrupted_depth: i32, info: &EvaluationType, counter_value: f64, elapsed_time: f64);
