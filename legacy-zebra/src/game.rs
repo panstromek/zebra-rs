e70467d417a1d109e3c41c29642c323412a7c441
panstromek <panstromek@seznam.cz>
2020-10-18 23:10:57 +0200

    mvoe ponder move to engine

--- legacy-zebra/src/game.rs
+++ legacy-zebra/src/game.rs
@@ -17,7 +17,7 @@ use engine::src::getcoeff::pattern_evaluation;
 use engine::src::myrandom::my_random;
 use engine::src::stubs::abs;
 use engine::src::osfbook::{get_book_move, get_candidate, get_candidate_count, fill_move_alternatives};
-use engine::src::game::{use_log_file, ComputeMoveLogger, ComputeMoveOutput, generic_compute_move, evaluated_list, game_evaluated_count, max_depth_reached, prefix_move, EvaluatedMove, compare_eval, CandidateMove, generic_game_init, BoardSource, FileBoardSource, engine_global_setup, MIDGAME_EVAL, WON_POSITION, UNDEFINED_EVAL, UNSOLVED_POSITION, EXACT_EVAL, WLD_EVAL, DRAWN_POSITION, LOST_POSITION, PASS_EVAL};
+use engine::src::game::{use_log_file, ComputeMoveLogger, ComputeMoveOutput, generic_compute_move, evaluated_list, game_evaluated_count, max_depth_reached, prefix_move, EvaluatedMove, compare_eval, CandidateMove, generic_game_init, BoardSource, FileBoardSource, engine_global_setup, MIDGAME_EVAL, WON_POSITION, UNDEFINED_EVAL, UNSOLVED_POSITION, EXACT_EVAL, WLD_EVAL, DRAWN_POSITION, LOST_POSITION, PASS_EVAL, PonderMoveReport};
 use std::ffi::CStr;
 use crate::src::thordb::LegacyThor;
 
@@ -130,165 +130,33 @@ pub unsafe fn game_init(file_name: *const i8, side_to_move: &mut i32) {
   with useful scores and moves.
 */
 
-pub unsafe fn ponder_move(side_to_move: i32,
-                                     _book: i32,
-                                     mid: i32,
-                                     exact: i32,
-                                     wld: i32) {
-    let mut eval_info =
-        EvaluationType{type_0: MIDGAME_EVAL,
-                       res: WON_POSITION,
-                       score: 0,
-                       confidence: 0.,
-                       search_depth: 0,
-                       is_book: 0,};
-    let mut entry =
-        HashEntry{key1: 0,
-                  key2: 0,
-                  eval: 0,
-                  move_0: [0; 4],
-                  draft: 0,
-                  selectivity: 0,
-                  flags: 0,};
-    let mut move_start_time: f64 = 0.;
-    let mut move_stop_time: f64 = 0.;
-    let mut i: i32 = 0;
-    let mut j: i32 = 0;
-    let mut this_move: i32 = 0;
-    let mut expect_count: i32 = 0;
-    let mut expect_list: [i32; 64] = [0; 64];
-    let mut best_pv: [i32; 61] = [0; 61];
-    /* Disable all time control mechanisms as it's the opponent's
-       time we're using */
-    toggle_abort_check(0 as i32);
-    toggle_midgame_abort_check(0 as i32);
-    start_move::<FE>(0 as i32 as f64,
-               0 as i32 as f64,
-               disc_count(0 as i32, &board) + disc_count(2 as i32, &board));
-    clear_ponder_times();
-    determine_hash_values(side_to_move, &board);
-    reset_counter(&mut nodes);
-    /* Find the scores for the moves available to the opponent. */
-    let mut hash_move = 0;
-    find_hash(&mut entry, 1 as i32);
-    if entry.draft as i32 != 0 as i32 {
-        hash_move = entry.move_0[0]
-    } else {
-        find_hash(&mut entry, 0 as i32);
-        if entry.draft as i32 != 0 as i32 {
-            hash_move = entry.move_0[0]
-        }
-    }
-    let stored_echo = echo;
-    echo = 0;
-    compute_move(side_to_move, 0 as i32, 0 as i32,
-                 0 as i32, 0 as i32, 0 as i32,
-                 if (8 as i32) < mid {
-                     8 as i32
-                 } else { mid }, 0 as i32, 0 as i32,
-                 0 as i32, &mut eval_info);
-    echo = stored_echo;
-    /* Sort the opponents on the score and push the table move (if any)
-       to the front of the list */
-    if force_return != 0 {
-        expect_count = 0 as i32
-    } else {
-        sort_moves(move_count[disks_played as usize]);
-        float_move(hash_move, move_count[disks_played as usize]);
-        expect_count = move_count[disks_played as usize];
-        i = 0;
+pub struct LibcPonderMoveReport;
+impl PonderMoveReport for LibcFatalError {
+    fn report_move_evals(expect_count: i32, move_list_item: &[i32; 64], evals_item: &[i32; 128]) {
+        let mut i = 0;
         while i < expect_count {
-            expect_list[i as usize] =
-                move_list[disks_played as usize][i as usize];
-            i += 1
-        }
-        report_hash_move(hash_move);
-        let move_list_item = &move_list[disks_played as usize];
-        let evals_item = &evals[disks_played as usize];
-        report_move_evals(expect_count, move_list_item, evals_item);
-    }
-    /* Go through the expected moves in order and prepare responses. */
-    let mut best_pv_depth = 0;
-    let mut i = 0;
-    while force_return == 0 && i < expect_count {
-        move_start_time = get_real_timer::<FE>();
-        set_ponder_move(expect_list[i as usize]);
-        this_move = expect_list[i as usize];
-        prefix_move = this_move;
-        make_move(side_to_move, this_move, 1 as i32);
-        compute_move(0 as i32 + 2 as i32 - side_to_move,
-                     0 as i32, 0 as i32, 0 as i32,
-                     1 as i32, 0 as i32, mid, exact, wld,
-                     0 as i32, &mut eval_info);
-        unmake_move(side_to_move, this_move);
-        clear_ponder_move();
-        move_stop_time = get_real_timer::<FE>();
-        add_ponder_time(expect_list[i as usize],
-                        move_stop_time - move_start_time);
-        ponder_depth[expect_list[i as usize] as usize] =
-            if ponder_depth[expect_list[i as usize] as usize] >
-                   max_depth_reached - 1 as i32 {
-                ponder_depth[expect_list[i as usize] as usize]
-            } else { (max_depth_reached) - 1 as i32 };
-        if i == 0 as i32 && force_return == 0 {
-            /* Store the PV for the first move */
-            best_pv_depth = pv_depth[0];
-            j = 0;
-            while j < pv_depth[0] {
-                best_pv[j as usize] =
-                    pv[0][j as usize];
-                j += 1
+            let move__ = move_list_item[i as usize];
+            let move_eval = evals_item[move__ as usize];
+            unsafe {
+                printf(b"%c%c %-6.2f  \x00" as *const u8 as *const i8,
+                       'a' as i32 + move__ % 10 as i32 - 1 as i32,
+                       '0' as i32 + move__ / 10 as i32, move_eval as f64 / 128.0f64);
             }
-        }
-        i += 1
-    }
-    /* Make sure the PV looks reasonable when leaving - either by
-       clearing it altogether or, preferrably, using the stored PV for
-       the first move if it is available. */
-    max_depth_reached += 1;
-    prefix_move = 0;
-    if best_pv_depth == 0 as i32 {
-        pv_depth[0] = 0 as i32
-    } else {
-        pv_depth[0] =
-            best_pv_depth + 1 as i32;
-        pv[0][0] =
-            expect_list[0];
-        i = 0;
-        while i < best_pv_depth {
-            pv[0][(i + 1 as i32) as usize] =
-                best_pv[i as usize];
+            if i % 7 as i32 == 6 as i32 || i == expect_count - 1 as i32 {
+                unsafe { puts(b"\x00" as *const u8 as *const i8); }
+            }
             i += 1
         }
     }
-    /* Don't forget to enable the time control mechanisms when leaving */
-    toggle_abort_check(1 as i32);
-    toggle_midgame_abort_check(1 as i32);
-}
 
-fn report_move_evals(expect_count: i32, move_list_item: &[i32; 64], evals_item: &[i32; 128]) {
-    let mut i = 0;
-    while i < expect_count {
-        let move__ = move_list_item[i as usize];
-        let move_eval = evals_item[move__ as usize];
+    fn report_hash_move(hash_move: i32) {
         unsafe {
-            printf(b"%c%c %-6.2f  \x00" as *const u8 as *const i8,
-                   'a' as i32 + move__ % 10 as i32 - 1 as i32,
-                   '0' as i32 + move__ / 10 as i32, move_eval as f64 / 128.0f64);
+            printf(b"%s=%d\n\x00" as *const u8 as *const i8,
+                   b"hash move\x00" as *const u8 as *const i8, hash_move);
         }
-        if i % 7 as i32 == 6 as i32 || i == expect_count - 1 as i32 {
-            unsafe { puts(b"\x00" as *const u8 as *const i8); }
-        }
-        i += 1
     }
 }
 
-fn report_hash_move(hash_move: i32) {
-    unsafe {
-        printf(b"%s=%d\n\x00" as *const u8 as *const i8,
-               b"hash move\x00" as *const u8 as *const i8, hash_move);
-    }
-}
 /*
   EXTENDED_COMPUTE_MOVE
   This wrapper on top of compute_move() calculates the evaluation
