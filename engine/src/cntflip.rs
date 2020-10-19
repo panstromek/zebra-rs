4b7e243c4aea96d6a273bd0b17562275d81d2ecc
panstromek <panstromek@seznam.cz>
2020-10-19 19:05:53 +0200

    make AnyFlips_compact safe

--- engine/src/cntflip.rs
+++ engine/src/cntflip.rs
@@ -5,43 +5,45 @@
 
    Last modified:   December 25, 1999
 */
-pub unsafe fn AnyFlips_compact(board: &mut [i32; 128], mut inc : &[i32],
+pub fn AnyFlips_compact(board: &mut [i32; 128], mut inc : &[i32],
                                sqnum: i32, color: i32, oppcol: i32) -> i32 {
 
-    let sq = &board[sqnum as usize];
+    let sq = sqnum;
     loop  {
         let any_drctnl_flips = {
-            let sq: &i32 = sq;
             let inc: i32 = inc[0];
-            let sq = sq as *const i32;
-            let mut pt = sq.offset(inc as isize);
-            if *pt == oppcol {
-                pt = pt.offset(inc as isize);
-                if *pt == oppcol {
-                    pt = pt.offset(inc as isize);
-                    if *pt == oppcol {
-                        pt = pt.offset(inc as isize);
-                        if *pt == oppcol {
-                            pt = pt.offset(inc as isize);
-                            if *pt == oppcol {
-                                pt = pt.offset(inc as isize);
-                                if *pt == oppcol { pt = pt.offset(inc as isize) }
+            let mut pt = sq + (inc);
+            if board[pt as usize] == oppcol {
+                pt = pt + (inc);
+                if board[pt as usize] == oppcol {
+                    pt = pt + (inc);
+                    if board[pt as usize] == oppcol {
+                        pt = pt + (inc);
+                        if board[pt as usize] == oppcol {
+                            pt = pt + (inc);
+                            if board[pt as usize] == oppcol {
+                                pt = pt + (inc);
+                                if board[pt as usize] == oppcol {
+                                    pt = pt + (inc)
+                                }
                             }
                         }
                     }
                 }
-                if *pt == color {
+                if board[pt as usize] == color {
                     return 1
                 }
             }
             0
-        };//  AnyDrctnlFlips(sq, *inc, color, oppcol);
+        };
 
         if any_drctnl_flips != 0 {
-            return 1 as i32
+            return 1
         }
-        inc = &inc[1..];// .offset(1);
-        if !(inc[0] != 0) { break ; }
+        inc = &inc[1..];
+        if !(inc[0] != 0) {
+            break;
+        }
     }
     0
 }
