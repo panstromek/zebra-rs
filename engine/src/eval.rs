8ceb26d13e25bc600cea17e81b7366a108908eb6
panstromek <panstromek@seznam.cz>
2020-10-22 20:01:52 +0200

    cleanup terminal_evaluation

--- engine/src/eval.rs
+++ engine/src/eval.rs
@@ -8,24 +8,24 @@ use crate::src::search::evaluations;
   Evaluates the position when no player has any legal moves.
 */
 
-pub unsafe fn terminal_evaluation(side_to_move: i32)
- -> i32 {
-    let mut disc_diff: i32 = 0;
-    let mut my_discs: i32 = 0;
-    let mut opp_discs: i32 = 0;
-    evaluations.lo = evaluations.lo.wrapping_add(1);
-    my_discs = piece_count[side_to_move as usize][disks_played as usize];
-    opp_discs =
-        piece_count[(0 as i32 + 2 as i32 - side_to_move) as
-                        usize][disks_played as usize];
-    if my_discs > opp_discs {
-        disc_diff = 64 as i32 - 2 as i32 * opp_discs
+pub unsafe fn terminal_evaluation(side_to_move: i32) -> i32 {
+    let eval_counter = &mut evaluations;
+    let my_discs = piece_count[side_to_move as usize][disks_played as usize];
+    let opp_discs = piece_count[(2 - side_to_move) as usize][disks_played as usize];
+
+    eval_counter.lo = eval_counter.lo.wrapping_add(1);
+    let disc_diff = if my_discs > opp_discs {
+         64 - 2 * opp_discs
     } else if opp_discs > my_discs {
-        disc_diff = 2 as i32 * my_discs - 64 as i32
-    } else { disc_diff = 0 as i32 }
-    if disc_diff > 0 as i32 {
-        return 29000 as i32 + disc_diff
-    } else if disc_diff == 0 as i32 {
-        return 0 as i32
-    } else { return -(29000 as i32) + disc_diff };
+         2 * my_discs - 64
+    } else {
+         0
+    };
+    return if disc_diff > 0 {
+        29000 + disc_diff
+    } else if disc_diff == 0 {
+        0
+    } else {
+        -29000 + disc_diff
+    };
 }
