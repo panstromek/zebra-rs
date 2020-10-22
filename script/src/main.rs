971259d0726e7f78e96c1ebd5b25f5a6942946be
panstromek <panstromek@seznam.cz>
2020-10-22 20:49:02 +0200

    group statics in engine/zebra into single Config struct with modified script

--- script/src/main.rs
+++ script/src/main.rs
@@ -1,7 +1,7 @@
 use regex::{Captures, Regex};
 
 fn main() {
-    let filename = "../engine/src/osfbook.rs";
+    let filename = "../engine/src/zebra.rs";
     let file = std::fs::read_to_string(filename).unwrap();
     let declaration = regex::Regex::new(
         r#"static\s+mut\s+([\w_]+):(.*?\s*?)=(.*?);\s*$"#).unwrap();
@@ -32,9 +32,9 @@ fn main() {
     let replacer = regex::Regex::new(&needle).unwrap();
     let mut struct_declaration = declarations
         .iter()
-        .fold(("pub struct Book {".to_string(), "pub static mut g_book : Book = Book {".to_string()),
+        .fold(("pub struct Config {".to_string(), "pub static mut config : Config = Config {".to_string()),
               |(struct_decl, static_decl), declaration| {
-                  (struct_decl + "\n" + declaration.name + ": " + declaration.type_ + ",",
+                  (struct_decl + "\npub " + declaration.name + ": " + declaration.type_ + ",",
                    static_decl + "\n" + declaration.name + ": " + declaration.default_value + ",")
               });
     struct_declaration.0 += "\n}\n";
@@ -43,16 +43,30 @@ fn main() {
     let new_lines = replace_in_file(&file, &declaration, &replacer, &mut struct_declaration, true);
     std::fs::write(filename, new_lines.join("\n")).unwrap();
 
-    let file = std::fs::read_to_string("../legacy-zebra/src/osfbook.rs").unwrap();
+    let file = std::fs::read_to_string("../legacy-zebra/src/zebra.rs").unwrap();
 
     let new_lines = replace_in_file(&file, &declaration, &replacer, &mut struct_declaration, false);
-    std::fs::write("../legacy-zebra/src/osfbook.rs", new_lines.join("\n")).unwrap();
+    std::fs::write("../legacy-zebra/src/zebra.rs", new_lines.join("\n")).unwrap();
 }
 
 fn replace_in_file(file: &String, declaration: &Regex, replacer: &Regex, struct_declaration: &mut (String, String), shoudl_write_decl: bool) -> Vec<String> {
     let mut decl_written = false;
+    let mut in_multiline_comment = false;
     let new_lines = file.lines()
         .map(|line| {
+            let is_use_statament = line.trim_start().starts_with("use ");
+
+            if line.contains("/*") {
+                in_multiline_comment = true;
+            }
+            if line.contains("*/") {
+                in_multiline_comment = false;
+                return line.to_string();
+            }
+            let is_comment = line.trim_start().starts_with("//");
+            if is_comment || in_multiline_comment {
+                return line.to_string()
+            }
             if declaration.is_match(line) {
                 return if decl_written || !shoudl_write_decl {
                     "".to_string()
@@ -61,13 +75,20 @@ fn replace_in_file(file: &String, declaration: &Regex, replacer: &Regex, struct_
                     struct_declaration.0.clone() + &struct_declaration.1
                 }
             }
-            let is_use_statament = line.trim_start().starts_with("use ");
-            return replacer.replace_all(line, |captures: &Captures| {
-                if is_use_statament {
-                    return "".to_string();
+            line.split("\"").enumerate().map(|(i, item )| {
+                if i % 2 == 0 {
+                    // not a string
+                    replacer.replace_all(item, |captures: &Captures| {
+                        if is_use_statament {
+                            // TODO find and replace comma
+                            return "".to_string();
+                        }
+                        return captures[1].to_string() + "config." + &captures[2] + &captures[3];
+                    }).to_string()
+                } else {
+                    item.to_string()
                 }
-                return captures[1].to_string() + "g_book." + &captures[2] + &captures[3];
-            }).to_string();
+            }).collect::<Vec<_>>().join("\"")
         })
         // .inspect(|x| println!("{}", x))
         .collect::<Vec<_>>();
