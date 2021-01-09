26332aaf6640e8b1edea0b468dc74fd4f677807c
panstromek <panstromek@seznam.cz>
2021-01-09 16:09:45 +0100

    run tests in parallel

--- run-tests.sh
+++ run-tests.sh
@@ -1,2 +1,2 @@
 ##!/bin/bash
- cargo build --bin zebra --release && cargo test --release --package tests -- --test-threads 1 --nocapture
+cargo build --bin zebra --release && cargo test --release --package tests -- --test-threads 6 --nocapture
