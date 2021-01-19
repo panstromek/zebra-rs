07319f621acdbe72742bdf4adaad63fd8444df5a
panstromek <panstromek@seznam.cz>
2021-01-19 21:59:07 +0100

    rewrite load_coeff_adjustments in safe rust

--- legacy-zebra/src/getcoeff.rs
+++ legacy-zebra/src/getcoeff.rs
@@ -1,10 +1,11 @@
-use libc_wrapper::{fclose, fscanf, fopen};
 use engine::src::getcoeff::{CoeffAdjustments};
 
 
 use crate::src::error::fatal_error;
 use std::ffi::CStr;
 use zlib_coeff_source::{ZLibSource, LoadError};
+use std::fs::File;
+use std::io::Error;
 
 pub fn new_z_lib_source(file_name: &CStr) -> ZLibSource {
     match ZLibSource::try_new(file_name) {
@@ -24,31 +25,12 @@ pub fn new_z_lib_source(file_name: &CStr) -> ZLibSource {
 }
 
 pub fn load_coeff_adjustments() -> Option<CoeffAdjustments> {
-    let adjust_stream = unsafe {
-        fopen(b"adjust.txt\x00" as *const u8 as *const i8,
-              b"r\x00" as *const u8 as *const i8)
-    };
-    if !adjust_stream.is_null() {
-        let mut disc_adjust = 0.0f64;
-        let mut edge_adjust = 0.0f64;
-        let mut corner_adjust = 0.0f64;
-        let mut x_adjust = 0.0f64;
-        unsafe {
-            fscanf(adjust_stream,
-                   b"%lf %lf %lf %lf\x00" as *const u8 as *const i8,
-                   &mut disc_adjust as *mut f64,
-                   &mut edge_adjust as *mut f64,
-                   &mut corner_adjust as *mut f64,
-                   &mut x_adjust as *mut f64);
-            fclose(adjust_stream);
-        }
-        Some(CoeffAdjustments {
-            disc_adjust,
-            edge_adjust,
-            corner_adjust,
-            x_adjust
-        })
-    } else {
-        None
-    }
+    let adjust_stream = std::fs::read_to_string("adjust.txt").ok()?;
+    let mut split = adjust_stream.split_whitespace();
+    Some(CoeffAdjustments {
+        disc_adjust: split.next()?.parse().ok()?,
+        edge_adjust: split.next()?.parse().ok()?,
+        corner_adjust: split.next()?.parse().ok()?,
+        x_adjust: split.next()?.parse().ok()?,
+    })
 }
