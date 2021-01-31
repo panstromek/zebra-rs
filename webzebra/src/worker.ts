95580720ed324207ee2c1aedee00ed662aff12d8
panstromek <panstromek@seznam.cz>
2021-01-31 22:18:59 +0100

    perf improvements (getting rid of unnecessary display_board calls)

--- webzebra/src/worker.ts
+++ webzebra/src/worker.ts
@@ -54,6 +54,7 @@ self.js_time = function js_time() {
 
 self.zebra = {
     display_board(arr) {
+        // TODO this call is really expensive for some reason, investigate that.
         self.postMessage([Message.DisplayBoard, arr])
     }
 }
