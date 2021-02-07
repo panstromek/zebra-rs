c8c2eb9c343e86fcc39bb7f7bc96f1c621a71db0
panstromek <panstromek@seznam.cz>
2021-02-07 13:54:27 +0100

    remove some redundant allows

--- legacy-zebra/lib.rs
+++ legacy-zebra/lib.rs
@@ -1,9 +1,7 @@
-#![allow(dead_code)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
 #![allow(clippy::missing_safety_doc)]
-#![allow(unused_unsafe)]
 #![allow(unused)]
 
 pub mod src {
