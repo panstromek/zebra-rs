b095b55d7e2a9e3e2b77519287f0f3c9b6a91929
panstromek <panstromek@seznam.cz>
2021-03-20 15:39:20 +0100

    build all targets when running tests

--- run-tests.sh
+++ run-tests.sh
@@ -1,2 +1,2 @@
 ##!/bin/bash
-cargo build --bin zebra --release && cargo test --release --package tests -- --test-threads 6 --nocapture
+cargo build --release && cargo test --release --package tests -- --test-threads 6 --nocapture
