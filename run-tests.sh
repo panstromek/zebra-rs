a5d76888fa2fc01bcc5eac219a476cc5c599131c
panstromek <panstromek@seznam.cz>
2021-05-02 15:26:37 +0200

    report coverage after running tests

--- run-tests.sh
+++ run-tests.sh
@@ -1,2 +1,9 @@
 ##!/bin/bash
-CARGO_TARGET_DIR=test-target RUSTFLAGS="-Zinstrument-coverage -C link-arg=-Wl,--wrap=time" cargo build --release && cargo test --release --package tests -- --test-threads 8 --nocapture
+CARGO_TARGET_DIR=test-target RUSTFLAGS="-Zinstrument-coverage -C link-arg=-Wl,--wrap=time" cargo build --release \
+&& cargo test --release --package tests -- --test-threads 8 --nocapture
+
+# Report coverage. Even if some tests fail.
+echo "-----"
+echo "Coverage:"
+cargo-profdata -- merge -sparse ./tests/snapshot-tests/*/*/default.profraw -o __all-tests-coverage.profdata\
+&& cargo cov -- report test-target/release/zebra -instr-profile __all-tests-coverage.profdata -ignore-filename-regex /home/matyas/.cargo/ | tail -n 1
