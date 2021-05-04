e9d7763724ecbba9e767f627c4c29091153ae450
panstromek <panstromek@seznam.cz>
2021-05-04 09:27:54 +0200

    expand run-tests script to add some more options to it

--- run-tests.sh
+++ run-tests.sh
@@ -1,9 +1,24 @@
 ##!/bin/bash
-CARGO_TARGET_DIR=test-target RUSTFLAGS="-Zinstrument-coverage -C link-arg=-Wl,--wrap=time" cargo build --release \
-&& cargo test --release --package tests -- --test-threads 8 --nocapture
+rm ./tests/snapshot-tests/*/*/default.profraw;
+while getopts t:c: flag
+do
+    case "${flag}" in
+        t) whichtests=${OPTARG};;
+        c) fullcoverage=${OPTARG};;
+        *)
+    esac
+done
 
+CARGO_TARGET_DIR=test-target RUSTFLAGS="-Zinstrument-coverage -C link-arg=-Wl,--wrap=time" cargo build --release \
+&& cargo test --release --package tests "$whichtests" -- --test-threads 8 --nocapture
+
+if test "$fullcoverage"; then
+  num_lines=100000
+else
+  num_lines=1
+fi
 # Report coverage. Even if some tests fail.
 echo "-----"
 echo "Coverage:"
 cargo-profdata -- merge -sparse ./tests/snapshot-tests/*/*/default.profraw -o __all-tests-coverage.profdata\
-&& cargo cov -- report test-target/release/zebra -instr-profile __all-tests-coverage.profdata -ignore-filename-regex /home/matyas/.cargo/ | tail -n 1
+&& cargo cov -- report test-target/release/zebra -instr-profile __all-tests-coverage.profdata -ignore-filename-regex /home/matyas/.cargo/ | tail -n "$num_lines"
