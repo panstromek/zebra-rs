f61268b1eaa0dee27b7d960f575b6add761a811a
panstromek <panstromek@seznam.cz>
2021-05-09 21:33:11 +0200

    remove safe realloc (it's not used anymore)

--- legacy-zebra/src/safemem.rs
+++ legacy-zebra/src/safemem.rs
@@ -22,20 +22,12 @@
 */
 
 use std::ffi::c_void;
-use libc_wrapper::{malloc, realloc};
+use libc_wrapper::malloc;
 #[macro_use]
 use crate::fatal_error;
 
 pub unsafe fn safe_malloc(size: u64) -> *mut c_void {
     let block = malloc(size);
-    if block.is_null() {
-        fatal_error!("{} {}\n", "Memory allocation failure when allocating", size);
-    }
-    block
-}
-
-pub unsafe fn safe_realloc(ptr: *mut c_void, size: u64) -> *mut c_void {
-    let block = realloc(ptr, size);
     if block.is_null() {
         fatal_error!("{} {}\n", "Memory allocation failure when allocating", size);
     }
