c39f227ec9e4bffc3e383b0bf59a0e27cd6c0086
panstromek <panstromek@seznam.cz>
2022-02-12 11:05:30 +0100

    add detailed static mut counts to stats script

--- unsafe-stats.sh
+++ unsafe-stats.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+echo unsafe counts:
 rg "unsafe[ \{]" ./ --count | rg "(.*)?:(.*)" -o -r "\$2 \$1" | sort --numeric
+echo static mut counts:
+rg "static mut" ./ --count | rg "(.*)?:(.*)" -o -r "\$2 \$1" | sort --numeric
 echo total unsafe:  $(rg "unsafe[ \{]" ./ | wc -l)
 echo total static mut: $(rg "static mut" ./ | wc -l)
