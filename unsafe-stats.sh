eaddbf7bfeb8512b1a996951813ac87c5f326d41
panstromek <panstromek@seznam.cz>
2022-02-16 07:35:37 +0100

    update unsafe stats a bit

--- unsafe-stats.sh
+++ unsafe-stats.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 echo unsafe counts:
 rg "unsafe[ \{]" ./ --count | rg "(.*)?:(.*)" -o -r "\$2 \$1" | sort --numeric
+echo
 echo static mut counts:
 rg "static mut" ./ --count | rg "(.*)?:(.*)" -o -r "\$2 \$1" | sort --numeric
+echo
 echo total unsafe:  $(rg "unsafe[ \{]" ./ | wc -l)
 echo total static mut: $(rg "static mut" ./ | wc -l)
