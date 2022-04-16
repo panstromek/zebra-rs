7819b4761c08dd91aaba8c765850e05ba8c1df4e
panstromek <panstromek@seznam.cz>
2022-04-16 09:51:49 +0200

    add simple link to source code repo

--- webzebra/src/App.vue
+++ webzebra/src/App.vue
@@ -1,4 +1,10 @@
 <template>
+  <div class="m-1.5 mb-2" style="width: 70px">
+    <a href="https://github.com/panstromek/zebra-rs" class="text-gray-200 text-xs" style="display: flex; align-items: center;" target="_blank">
+      <img src="/GitHub-Mark-Light-32px.png" alt="Github Icon" width="16" height="16" class="mr-2">
+      Source
+    </a>
+  </div>
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
