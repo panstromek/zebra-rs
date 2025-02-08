13d471c5159a00ce045e2f125a433ae826b1cbb9
panstromek <panstromek@seznam.cz>
2025-02-08 17:02:39 +0100

    ref: move github link out of vue file to index.html

--- webzebra/src/App.vue
+++ webzebra/src/App.vue
@@ -1,10 +1,4 @@
 <template>
-  <div class="m-1.5 mb-2" style="width: 70px">
-    <a href="https://github.com/panstromek/zebra-rs" class="text-gray-200 text-xs" style="display: flex; align-items: center;" target="_blank">
-      <img src="/GitHub-Mark-Light-32px.png" alt="Github Icon" width="16" height="16" class="mr-2">
-      Source
-    </a>
-  </div>
   <HelloWorld />
 </template>
 
