3696f361df2acf7d2249407131d923872cc56e34
panstromek <panstromek@seznam.cz>
2025-02-08 17:11:03 +0100

    ref: extract game related UI functions out of the vue component

--- webzebra/src/components/HelloWorld.vue
+++ webzebra/src/components/HelloWorld.vue
@@ -90,91 +90,12 @@ import {defineComponent} from 'vue'
 import ZebraWorker from '../worker.ts?worker=true'
 import {EvaluatedMove, Message, MessageType, SkillSetting} from "../message";
 import {createStopToken, stop} from "../stopToken";
+import {boardData, Circle, Eval, scoreFromCircles} from "../game";
 
 interface ZWorker extends Worker {
   postMessage(message: Message): void
 }
 
-type Circle = {
-  cx: number,
-  cy: number,
-  r: number,
-  color: string
-}
-type Eval = {
-  x: number,
-  y: number,
-  color: string,
-  text: string
-}
-
-function boardData(board: number[], clickedMove: number | undefined, evaluatedMoves: EvaluatedMove[]) {
-  let evals = []
-  const fieldSize = 100
-  const arr = [] as Circle[]
-  for (let i = 1; i <= 8; i++) {
-    for (let j = 1; j <= 8; j++) {
-      let color;
-      let move = 10 * i + j;
-
-      switch (board[move]) {
-        case 0  :
-          color = 'black'
-          break;
-
-        case 2 :
-          color = 'white'
-          break;
-
-        default : {
-          if (clickedMove === move) {
-            // todo take into account side to move
-            color = 'rgba(127, 127, 127, 0.5)'
-            break
-          }
-          const eval_ = evaluatedMoves.find((eval_: EvaluatedMove) => eval_.move === move)
-          if (!eval_) {
-            continue
-          }
-
-          if (eval_.best) {
-            color = '#00FFFF'
-          } else {
-            color = '#FFFF00'
-          }
-          const text = eval_.eval_s
-          evals.push({
-            x: (j - 1) * fieldSize + 0.2 * fieldSize,
-            y: (i - 1) * fieldSize + 0.65 * fieldSize,
-            color,
-            text
-          })
-          continue
-        }
-      }
-
-      const pieceSize = 80
-      arr.push({
-        cx: (j - 1) * fieldSize + 0.5 * fieldSize,
-        cy: (i - 1) * fieldSize + 0.5 * fieldSize,
-        r: pieceSize / 2,
-        color
-      })
-    }
-  }
-  return {
-    circles: arr,
-    evals
-  }
-}
-
-const scoreFromCircles = (circles: Circle[]) => {
-  return {
-    // todo this is slow, use something better when we use some more efficient board representation
-    white: circles.reduce((count: number, circle: Circle) => circle.color === 'white' ? count + 1 : count, 0),
-    black: circles.reduce((count: number, circle: Circle) => circle.color === 'black' ? count + 1 : count, 0)
-  }
-}
 export default defineComponent({
   name: 'HelloWorld',
   data() {
---
+++ webzebra/src/game.ts
@@ -0,0 +1,82 @@
+import {EvaluatedMove} from "./message";
+
+export type Circle = {
+    cx: number,
+    cy: number,
+    r: number,
+    color: string
+}
+export type Eval = {
+    x: number,
+    y: number,
+    color: string,
+    text: string
+}
+
+export function boardData(board: number[], clickedMove: number | undefined, evaluatedMoves: EvaluatedMove[]) {
+    let evals = []
+    const fieldSize = 100
+    const arr = [] as Circle[]
+    for (let i = 1; i <= 8; i++) {
+        for (let j = 1; j <= 8; j++) {
+            let color;
+            let move = 10 * i + j;
+
+            switch (board[move]) {
+                case 0  :
+                    color = 'black'
+                    break;
+
+                case 2 :
+                    color = 'white'
+                    break;
+
+                default : {
+                    if (clickedMove === move) {
+                        // todo take into account side to move
+                        color = 'rgba(127, 127, 127, 0.5)'
+                        break
+                    }
+                    const eval_ = evaluatedMoves.find((eval_: EvaluatedMove) => eval_.move === move)
+                    if (!eval_) {
+                        continue
+                    }
+
+                    if (eval_.best) {
+                        color = '#00FFFF'
+                    } else {
+                        color = '#FFFF00'
+                    }
+                    const text = eval_.eval_s
+                    evals.push({
+                        x: (j - 1) * fieldSize + 0.2 * fieldSize,
+                        y: (i - 1) * fieldSize + 0.65 * fieldSize,
+                        color,
+                        text
+                    })
+                    continue
+                }
+            }
+
+            const pieceSize = 80
+            arr.push({
+                cx: (j - 1) * fieldSize + 0.5 * fieldSize,
+                cy: (i - 1) * fieldSize + 0.5 * fieldSize,
+                r: pieceSize / 2,
+                color
+            })
+        }
+    }
+    return {
+        circles: arr,
+        evals
+    }
+}
+
+export const scoreFromCircles = (circles: Circle[]) => {
+    return {
+        // todo this is slow, use something better when we use some more efficient board representation
+        white: circles.reduce((count: number, circle: Circle) => circle.color === 'white' ? count + 1 : count, 0),
+        black: circles.reduce((count: number, circle: Circle) => circle.color === 'black' ? count + 1 : count, 0)
+    }
+}
