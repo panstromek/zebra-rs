995ec71e11320b033f047b44e5aa6263df13d8d6
panstromek <panstromek@seznam.cz>
2025-02-08 17:01:22 +0100

    ref: mv styles out of main vue

--- webzebra/src/App.vue
+++ webzebra/src/App.vue
@@ -19,16 +19,3 @@ export default defineComponent({
   }
 })
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-body {
-  background-color: #3a7f46
-}
-</style>
