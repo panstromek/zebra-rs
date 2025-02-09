970c493e12c5ce2f4f7ccf670390b8b7e19c5325
panstromek <panstromek@seznam.cz>
2025-02-09 12:40:32 +0100

    ref: inline

--- webzebra/src/game.ts
+++ webzebra/src/game.ts
@@ -5,10 +5,3 @@ export type Circle = {
     color: string
 }
 
-export const scoreFromCircles = (circles: Circle[]) => {
-    return {
-        // todo this is slow, use something better when we use some more efficient board representation
-        white: circles.reduce((count: number, circle: Circle) => circle.color === 'white' ? count + 1 : count, 0),
-        black: circles.reduce((count: number, circle: Circle) => circle.color === 'black' ? count + 1 : count, 0)
-    }
-}
--- webzebra/src/main.ts
+++ webzebra/src/main.ts
@@ -1,7 +1,7 @@
 import './index.css'
 import {EvaluatedMove, Message, MessageType, SkillSetting} from "./message";
 import {createStopToken, stop} from "./stopToken";
-import {Circle, scoreFromCircles} from "./game";
+import {Circle} from "./game";
 
 interface ZWorker extends Worker {
     postMessage(message: Message): void
@@ -191,7 +191,11 @@ function render() {
     }
 
     const circles = arr
-    const score = scoreFromCircles(circles)
+    const score = {
+        // todo this is slow, use something better when we use some more efficient board representation
+        white: circles.reduce((count: number, circle: Circle) => circle.color === 'white' ? count + 1 : count, 0),
+        black: circles.reduce((count: number, circle: Circle) => circle.color === 'black' ? count + 1 : count, 0)
+    }
 
     const circlesHtml = circles.map(circle => {
         return `<circle r="${circle.r}" cx="${circle.cx}" cy="${circle.cy}" style="fill: ${circle.color}"></circle>`
